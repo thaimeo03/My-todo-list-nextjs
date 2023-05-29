465e38193caf3784bd61e3e7057044681907864c
THTmeo <thtgenshin@gmail.com>
2023-05-29 16:38:54 +0700

    change UI todolist siide

--- app/components/TodoListSide/TodoCard/TodoCard.tsx
+++ app/components/TodoListSide/TodoCard/TodoCard.tsx
@@ -1,14 +1,33 @@
+"use client"
 import { ImBin2 } from "react-icons/im"
 import { AiFillEdit } from "react-icons/ai"
+import { BsThreeDots, BsCalendarDate } from "react-icons/bs"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "../../ui/dropdown-menu"
+import { Calendar } from "../../ui/calendar"
+import { useCallback, useState } from "react"
+import { Popover, PopoverContent, PopoverTrigger } from "../../ui/popover"
 
 interface Props {
-    isChecked: boolean;
     workName: string;
 }
 
-export default function TodoCard({ isChecked, workName }: Props) {
+export default function TodoCard({ workName }: Props) {
+    const [isChecked, setIsChecked] = useState(false)
+    const [date, setDate] = useState<Date | undefined>(new Date())
+    console.log(date?.toISOString())
+
+    const handleToggle = useCallback(() => {
+        setIsChecked(!isChecked)
+    }, [isChecked])
+
     return (
-        <li className="p-3 bg-[#21212b] rounded-xl w-full flex mt-4 justify-between">
+        <li className={`p-3 ${isChecked ? "bg-[#4821d4]" : "bg-[#21212b] "} rounded-xl w-full flex mt-4 justify-between`}>
             <div className="flex space-x-3">
                 <div>
                     {isChecked ? (
@@ -16,6 +35,7 @@ export default function TodoCard({ isChecked, workName }: Props) {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 448 512"
                             className="w-6 h-6 fill-[#fc76a1] cursor-pointer"
+                            onClick={handleToggle}
                         >
                             <path d="M64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V96c0-8.8-7.2-16-16-16H64zM0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
                         </svg>
@@ -24,16 +44,45 @@ export default function TodoCard({ isChecked, workName }: Props) {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 448 512"
                             className="w-6 h-6 fill-[#fc76a1] cursor-pointer"
+                            onClick={handleToggle}
                         >
                             <path d="M384 80c8.8 0 16 7.2 16 16V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V96c0-8.8 7.2-16 16-16H384zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z" />
                         </svg>
                     )}
                 </div>
-                <p>{workName}</p>
+                <p className="max-w-[95%]">{workName}</p>
             </div>
             <div className="flex space-x-2">
-                <AiFillEdit size={20} cursor="pointer" color="#08C1F9" />
-                <ImBin2 size={17} cursor="pointer" color="#FA3333" />
+                <Popover>
+                    <PopoverTrigger>
+                        <BsCalendarDate />
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <Calendar
+                            mode="single"
+                            selected={date}
+                            onSelect={setDate}
+                            className="rounded-md"
+                        />
+                    </PopoverContent>
+                </Popover>
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <BsThreeDots />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="bg-[#21212b]">
+                        <DropdownMenuItem className="flex items-center space-x-2 hover:bg-[#272732]">
+                            <AiFillEdit size={22} cursor="pointer" color="white" />
+                            <p className="cursor-pointer text-white w-full">Edit</p>
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator className="bg-gray-500" />
+                        <DropdownMenuItem className="flex items-center space-x-3 hover:bg-[#272732]">
+                            <ImBin2 size={16} cursor="pointer" color="white" />
+                            <p className="cursor-pointer text-white w-full">Delete</p>
+                        </DropdownMenuItem>
+
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </li>
     );
--- app/components/TodoListSide/TodoListSide.tsx
+++ app/components/TodoListSide/TodoListSide.tsx
@@ -9,57 +9,59 @@ export default function TodoListSide() {
 
     return (
         <div className="col-span-10 bg-[#181820]">
-            <div className="w-[60%] mx-auto mt-10 text-white">
-                <div className="flex justify-between items-center">
-                    <p className="text-xl font-bold"></p>
-                    <div className="cursor-pointer">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+            {
+                todoListId && (
+                    <div className="w-[60%] mx-auto mt-10 text-white">
+                        <div className="flex justify-between items-center">
+                            <p className="text-xl font-bold"></p>
+                            <div className="cursor-pointer">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+
+                        <div className="mt-9 p-3 border border-gray-700 rounded-2xl flex items-center">
+                            <button
+                                type="button"
+                                className="bg-[#fc76a1] w-7 h-7 grid place-items-center rounded-xl cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed font-normal"
+                                disabled={false}
+                            >
+                                +
+                            </button>
+                            <input
+                                type="text"
+                                className="ml-3 text-white placeholder:opacity-60 text-sm w-full outline-none bg-transparent"
+                                placeholder="Add a task"
+                                name="work"
                             />
-                        </svg>
+                        </div>
+
+                        <div className="mt-9">
+                            <p className="capitalize">Task list</p>
+                            <ul className="mt-4">
+                                <TodoCard
+                                    workName="Lorem ipsum dolor sit, amet consectetur adipisicing elit"
+                                />
+                                <TodoCard
+                                    workName="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit ea illo eligendi inventore dolor enim nihil obcaecati culpa impedit id voluptate error neque fugiat perspiciatis doloribus, perferendis saepe vel recusandae"
+                                />
+                            </ul>
+                        </div>
                     </div>
-                </div>
-
-                <div className="mt-9 p-3 border border-gray-700 rounded-2xl flex items-center">
-                    <button
-                        type="button"
-                        className="bg-[#fc76a1] w-7 h-7 grid place-items-center rounded-xl cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed font-normal"
-                        disabled={false}
-                    >
-                        +
-                    </button>
-                    <input
-                        type="text"
-                        className="ml-3 text-white placeholder:opacity-60 text-sm w-full outline-none bg-transparent"
-                        placeholder="Add a task"
-                        name="work"
-                    />
-                </div>
-
-                <div className="mt-9">
-                    <p className="capitalize">Task list</p>
-                    <ul className="mt-4">
-                        <TodoCard
-                            isChecked={false}
-                            workName="Lorem ipsum dolor sit, amet consectetur adipisicing elit"
-                        />
-                        <TodoCard
-                            isChecked={true}
-                            workName="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit ea illo eligendi inventore dolor enim nihil obcaecati culpa impedit id voluptate error neque fugiat perspiciatis doloribus, perferendis saepe vel recusandae?"
-                        />
-                    </ul>
-                </div>
-            </div>
+                )
+            }
         </div>
     )
 }
